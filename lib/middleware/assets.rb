a5bcc1a2780970f46b9c936feb867eb8fe4659b8
xprazak2 <oprazak@redhat.com>
2015-10-10 14:13:09 +0200

    progress

--- lib/middleware/assets.rb
+++ lib/middleware/assets.rb
@@ -21,8 +21,9 @@ module Middleware
         config.environment = assets
         config.prefix = "/assets"
         config.digest = false
+        #config.expand = true # we cannot expand because sprockets add .self to asset path making it fail
         config.public_path = public_folder
-        config.debug = true if development? #do not turn on, causes stylesheet load to fail!!!
+        config.debug = true if development? #do not turn on, sprockets bug causes asset load failure
       end
     end
 
