b7b0ef23c6ecdbb7ffe9f3d3908f5f707c16b62d
Ondrej Prazak <o.prazak5@seznam.cz>
2015-11-13 17:24:29 +0100

    progress

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-bower-task');
+
+  grunt.initConfig({
+    bower: {
+      install: {
+        options: {
+          targetDir: "public",
+          verbose: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', 'A default task', function () {
+    grunt.log.writeln('Running empty task by default...done');
+  });
+};
