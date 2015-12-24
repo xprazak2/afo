724a69226d00244389330df3d7f00fc1aa264532
Ondrej Prazak <o.prazak5@seznam.cz>
2015-12-24 15:22:13 +0100

    factory fix

--- test/factories.rb
+++ test/factories.rb
@@ -10,6 +10,7 @@ FactoryGirl.define do
     sequence(:path) { |n| "/public/comic/#{n}" }
     file nil
     created_at Time.utc(2015, 12, 24, 17, 49)
+    updated_at Time.utc(2015, 12, 24, 17, 49)
   end
 
   factory :content, :class => Afo::Content do
