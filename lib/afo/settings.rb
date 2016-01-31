c612418c049437dc16c42e52c9cf967625b4ca30
Ondrej Prazak <o.prazak5@seznam.cz>
2016-01-31 01:48:28 +0100

    settings

--- lib/afo/settings.rb
+++ lib/afo/settings.rb
@@ -1,5 +1,6 @@
 require 'yaml'
 require 'logging'
+require 'loader'
 
 module Afo
   class Settings
@@ -21,10 +22,7 @@ module Afo
     end
 
     if ENV['RACK_ENV'] == 'production' && File.exists?("#{APP_ROOT}/config/database.yml")
-      db_settings = YAML.load_file("#{APP_ROOT}/config/database.yml")
-      @@settings[:db_user] = db_settings['username']
-      @@settings[:db_password] = db_settings['password']
-      @@settings[:db_path] = "#{db_settings['host']}:#{db_settings['port']}/#{db_settings['database']}"
+      Loader.new(@@settings).load_db_config
     end
     logger.info "Initialized with settings:"
     logger.info @@settings
